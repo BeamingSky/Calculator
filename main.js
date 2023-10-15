037e4b1091845f6322d827ec589183978b6ed4c6
BeamingSky <133656864+BeamingSky@users.noreply.github.com>
2023-10-15 20:06:33 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,40 +1,53 @@
 // Get the display element by its ID
-let display = document.getElementById("display");
+      let display = document.getElementById("display");
 
-// Function to add a value to the display
-function addToDisplay(value) {
-  if (value === "%") {
-    try {
-      // If the value is '%', evaluate the current display value as a percentage
-      const currentValue = parseFloat(display.value);
-      
-      if (!isNaN(currentValue)) {
-        display.value = (currentValue / 100).toString();
-      } 
-      else {
-        throw "Invalid Input"; // Throw an error for invalid input
+      // Function to add a value to the display
+      function addToDisplay(value) {
+        if (value === "%") {
+          try {
+            // Check if the current display value has exactly two numbers separated by '*'
+            const match = display.value.match(/(\d+)\s*\*\s*(\d+)/);
+
+            if (match) {
+              const num1 = parseFloat(match[1]);
+              const num2 = parseFloat(match[2]);
+
+              if (!isNaN(num1) && !isNaN(num2)) {
+                const result = (num1 * num2) / 100;
+                display.value = result.toString();
+              } else {
+                throw "Invalid";
+              }
+            } else {
+              throw "Invalid";
+            }
+          } catch (error) {
+            display.value = "Invalid Input";
+          }
+        } else {
+          // Concatenate the provided value to the current display value
+          display.value += value;
+        }
       }
-    } 
-    catch (error) {
-      display.value = "Error: " + error; // Handle and display the error
-    }
-  } 
-  else {
-    // Concatenate the provided value to the current display value
-    display.value += value;
-  }
-}
 
-// Function to perform calculation
-function calculate() {
-  try {
-    display.value = eval(display.value); // Evaluate the current display value as an expression
-  } catch (error) {
-    display.value = "Error: " + error; // Handle and display the error
-  }
-}
+      function calculate() {
+        try {
+          display.value = eval(display.value); // Evaluate the current display value as an expression
+        } catch (error) {
+          display.value = "Invalid Input"; // Handle and display the error
+        }
+      }
 
-// Function to clear the display
-function clearDisplay() {
-  display.value = ""; // Set the display value to an empty string, clearing the content
-}
+      // Function to delete the last character from the display
+      function del() {
+        let str = "";
+        for (let i = 0; i < display.value.length - 1; i++) {
+          str += display.value[i];
+        }
+        console.log(str);
+        display.value = str;
+      }
+
+      function clearDisplay() {
+        display.value = "";
+      }
